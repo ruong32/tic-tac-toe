ab3ca9f81eb6f87a3745d5711a0ad010692296eb
ruong <ruong@192.168.1.7>
2024-05-30 10:50:21 +0700

    fix: overflow y

--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -4,7 +4,7 @@ import { GameBoard } from './game-board'
 import { ScoreBoard } from './score-board'
 
 export const Game = withGameContext(() => (
-  <Container className="py-16 h-full flex flex-col items-center gap-10 text-white">
+  <Container className="py-16 h-full flex flex-col items-center gap-10 text-white overflow-auto">
     <ScoreBoard />
     <GameBoard />
   </Container>
